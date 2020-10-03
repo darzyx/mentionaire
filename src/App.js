d2d7307f46551bf09e11a5af66d2898f0396c1c3
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2020-10-02 22:09:44 -0400

    Create icon toolbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const IconToolbar = styled.div`
+const IconToolbarWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
@@ -16,23 +16,29 @@ const IconToolbar = styled.div`
   }
 `;
 
+function IconToolbar() {
+  return (
+    <IconToolbarWrapper>
+      <i className="fab fa-google" />
+      <a
+        href="https://twitter.com/search-advanced"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-twitter" />
+      </a>
+      <i className="fab fa-reddit" />
+      <i className="fab fa-facebook" />
+      <i className="fab fa-linkedin" />
+    </IconToolbarWrapper>
+  );
+}
+
 function App() {
   return (
     <div className="app">
       <h1>Mentionaire</h1>
-      <IconToolbar>
-        <i className="fab fa-google" />
-        <a
-          href="https://twitter.com/search-advanced"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fab fa-twitter" />
-        </a>
-        <i className="fab fa-reddit" />
-        <i className="fab fa-facebook" />
-        <i className="fab fa-linkedin" />
-      </IconToolbar>
+      <IconToolbar />
     </div>
   );
 }
