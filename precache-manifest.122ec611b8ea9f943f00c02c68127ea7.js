d4a7e8d66c30dd9bff2300b00760d30806daae66
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2020-10-02 20:48:30 -0400

    Updates

---
+++ precache-manifest.122ec611b8ea9f943f00c02c68127ea7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2900098244b39d38595b44ab08fb11f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e793395910d7185892f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee5c806c3321a6963e11",
+    "url": "/static/js/2.d4171cd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4171cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e793395910d7185892f",
+    "url": "/static/js/main.d2229541.chunk.js"
+  },
+  {
+    "revision": "4333637601e08b45452c",
+    "url": "/static/js/runtime-main.927d835b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
