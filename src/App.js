e03cbf6e9f5fe692f739ad32182bc05fb691fe82
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2020-10-02 21:53:33 -0400

    Install styled-components, add icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const IconToolbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 300px;
+
+  i {
+    font-size: 20px;
+    cursor: pointer;
+  }
+`;
 
 function App() {
   return (
     <div className="app">
       <h1>Mentionaire</h1>
-      <p>Hello, world.</p>
+      <IconToolbar>
+        <i className="fab fa-google" />
+        <i className="fab fa-twitter" />
+        <i className="fab fa-reddit" />
+        <i className="fab fa-facebook" />
+        <i className="fab fa-linkedin" />
+      </IconToolbar>
     </div>
   );
 }
